d5c41cb5dbbeb671ac77ac577c6bb70048ce8282
NovemberMeme <juamibenito8@gmail.com>
2021-02-11 20:56:47 +0800

    Initial Commit

---
+++ ProjectTowerOfHanoi_Unity/Assets/_ProjectTowerOfHanoi/Scripts/DataBaseSO.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerOfHanoi
+{
+    [CreateAssetMenu(fileName = "New Database ScriptableObject", menuName = "New Database SO")]
+    public class DataBaseSO : ScriptableObject
+    {
+        public Vector3 RodPos1;
+        public Vector3 RodPos2;
+        public Vector3 RodPos3;
+
+        public int DiskCount = 3;
+        public List<Disk> AllDisks = new List<Disk>();
+
+        public float SizeScaleFactor = 1.2f;
+        public float DistanceScaleFactor = 0.1f;
+        public float Spacing = 1;
+
+        public void PickUp(Disk _disk)
+        {
+
+        }
+
+        public void Place(Disk _disk)
+        {
+
+        }
+
+        private void GetPickUpDistance()
+        {
+            for (int i = 0; i < DiskCount; i++)
+            {
+
+            }
+        }
+    }
+}
---
+++ ProjectTowerOfHanoi_Unity/Assets/_ProjectTowerOfHanoi/Scripts/Disk.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace TowerOfHanoi
+{
+    public class Disk : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+    {
+        public int MyRodIndex;
+
+        public DataBaseSO DB;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Debug.Log(eventData.pointerPress.name);
+
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            Debug.Log(eventData.pointerEnter.name);
+
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            Debug.Log("Exited");
+
+        }
+    }
+}
